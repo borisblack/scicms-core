dbc910f07bcef8675a06d7a29a9bc8929d8aaad6
Boris Chernysh <bchernysh@lanit.ru>
2022-04-29 12:06:05 +0700

    Add Base64EncodingTest

--- src/main/kotlin/ru/scisolutions/scicmscore/util/Base64Encoding.kt
+++ src/main/kotlin/ru/scisolutions/scicmscore/util/Base64Encoding.kt
@@ -3,26 +3,11 @@ package ru.scisolutions.scicmscore.util
 import com.google.common.io.BaseEncoding
 
 object Base64Encoding {
-    fun encodeNullable(rawText: String?) =
-        if (rawText == null)
-            null
-        else
-            try {
-                encode(rawText)
-            } catch (e: Exception) {
-                rawText
-            }
+    fun encodeNullable(rawText: String?) = if (rawText.isNullOrBlank()) null else encode(rawText)
 
     fun encode(rawText: String) = BaseEncoding.base64().encode(rawText.toByteArray())
 
-    fun decodeNullable(encodedText: String?) =
-        if (encodedText == null) null
-        else
-            try {
-                decode(encodedText)
-            } catch (e: Exception) {
-                encodedText
-            }
+    fun decodeNullable(encodedText: String?) = if (encodedText.isNullOrBlank()) null else decode(encodedText)
 
-    fun decode(encodedText: String) = BaseEncoding.base64().decode(encodedText)
+    fun decode(encodedText: String) = BaseEncoding.base64().decode(encodedText).decodeToString()
 }
