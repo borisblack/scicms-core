184b5e06d447dc72ef0c58e2d09cd20fd462a0a9
Boris Chernysh <bchernysh@lanit.ru>
2023-01-29 16:24:47 +0700

    Fix extended dataset implementation according to test results

--- src/main/kotlin/ru/scisolutions/scicmscore/engine/dao/impl/DatasetDaoImpl.kt
+++ src/main/kotlin/ru/scisolutions/scicmscore/engine/dao/impl/DatasetDaoImpl.kt
@@ -5,14 +5,12 @@ import org.springframework.stereotype.Service
 import ru.scisolutions.scicmscore.config.PersistenceConfig.JdbcTemplateMap
 import ru.scisolutions.scicmscore.engine.dao.DatasetDao
 import ru.scisolutions.scicmscore.engine.db.DatasetRowMapper
-import ru.scisolutions.scicmscore.engine.db.query.DatasetQueryBuilder
 import ru.scisolutions.scicmscore.engine.db.query.DatasetSqlParameterSource
 import ru.scisolutions.scicmscore.persistence.entity.Dataset
 
 @Service
 class DatasetDaoImpl(
-    private val jdbcTemplateMap: JdbcTemplateMap,
-    private val datasetQueryBuilder: DatasetQueryBuilder
+    private val jdbcTemplateMap: JdbcTemplateMap
 ) : DatasetDao {
     override fun load(dataset: Dataset, sql: String, paramSource: DatasetSqlParameterSource): List<Map<String, Any?>> {
         logger.debug("Running SQL: {}", sql)
--- src/main/kotlin/ru/scisolutions/scicmscore/engine/model/input/PaginationInput.kt
+++ src/main/kotlin/ru/scisolutions/scicmscore/engine/model/input/PaginationInput.kt
@@ -7,12 +7,18 @@ class PaginationInput(
     val limit: Int? = null
 ) {
     companion object {
-        fun fromMap(map: Map<String, Int>) =
-            PaginationInput(
-                page = map["page"],
-                pageSize = map["pageSize"],
-                start = map["start"],
-                limit = map["limit"]
+        fun fromMap(map: Map<String, Any>): PaginationInput {
+            val page = map["page"]
+            val pageSize = map["pageSize"]
+            val start = map["start"]
+            val limit = map["limit"]
+
+            return PaginationInput(
+                page = if (page is String) page.toInt() else page as Int?,
+                pageSize = if (pageSize is String) pageSize.toInt() else pageSize as Int?,
+                start = if (start is String) start.toInt() else start as Int?,
+                limit = if (limit is String) limit.toInt() else limit as Int?
             )
+        }
     }
 }
