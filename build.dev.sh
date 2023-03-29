f4ce8503e9f0600eb4e6e61b7f24e1b423447dbd
Boris Chernysh <bchernysh@lanit.ru>
2023-03-29 15:59:13 +0700

    Remove extra files

--- build.dev.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -xe
-
-export COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1
-docker-compose -f "docker-compose.development.yml" -p scicms-core--development down  --remove-orphans
-docker-compose -f "docker-compose.development.yml" -p scicms-core--development build --no-cache
-docker-compose -f "docker-compose.development.yml" -p scicms-core--development up -d
