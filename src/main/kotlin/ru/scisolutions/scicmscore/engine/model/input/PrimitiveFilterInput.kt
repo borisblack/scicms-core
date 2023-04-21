525f01d69aafb2d5bbd56461b69971620e6f349c
Boris Chernysh <bchernysh@lanit.ru>
2023-04-21 14:16:42 +0700

    Refactor dataset null/notNull filters mapping

--- src/main/kotlin/ru/scisolutions/scicmscore/engine/model/input/PrimitiveFilterInput.kt
+++ src/main/kotlin/ru/scisolutions/scicmscore/engine/model/input/PrimitiveFilterInput.kt
@@ -16,8 +16,8 @@ class PrimitiveFilterInput(
     val betweenFilter: BetweenPair?,
     val inFilter: List<Any?>?,
     val notInFilter: List<Any?>?,
-    val nullFilter: Any?,
-    val notNullFilter: Any?,
+    val nullFilter: Boolean?,
+    val notNullFilter: Boolean?,
     andFilterList: List<PrimitiveFilterInput>?,
     orFilterList: List<PrimitiveFilterInput>?,
     notFilter: PrimitiveFilterInput?
@@ -52,8 +52,8 @@ class PrimitiveFilterInput(
                 if (it is List<*>) it.filterNotNull() else throw IllegalArgumentException("Invalid NOT IN filter")
             },
 
-            nullFilter = filters["${opPrefix}$NULL_KEY"],
-            notNullFilter = filters["${opPrefix}$NOT_NULL_KEY"],
+            nullFilter = filters["${opPrefix}$NULL_KEY"]?.let { it == true || it == "true" },
+            notNullFilter = filters["${opPrefix}$NOT_NULL_KEY"]?.let { it == true || it == "true" },
 
             andFilterList = filters["${opPrefix}$AND_KEY"]?.let { list ->
                 if (list is List<*>) {
