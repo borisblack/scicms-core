b17c7503aa5377097dd0e4b195b08c2874eaab2b
Boris Chernysh <chernyshba@iss-reshetnev.ru>
2023-06-28 09:51:27 +0700

    Add data config field in GraphQL API

--- src/main/kotlin/ru/scisolutions/scicmscore/api/graphql/datafetcher/query/ConfigDataFetcher.kt
+++ src/main/kotlin/ru/scisolutions/scicmscore/api/graphql/datafetcher/query/ConfigDataFetcher.kt
@@ -2,18 +2,28 @@ package ru.scisolutions.scicmscore.api.graphql.datafetcher.query
 
 import com.netflix.graphql.dgs.DgsComponent
 import com.netflix.graphql.dgs.DgsQuery
+import ru.scisolutions.scicmscore.config.props.DataProps
 import ru.scisolutions.scicmscore.config.props.I18nProps
 
 @DgsComponent
 class ConfigDataFetcher(
+    private val dataProps: DataProps,
     private val i18nProps: I18nProps
 ) {
     class Config(
+        val data: DataConfig,
         val i18n: I18nProps
+    )
+
+    class DataConfig(
+        val dataSources: Set<String>
     )
 
     @DgsQuery
     fun config(): Config? = Config(
+        data = DataConfig(
+            dataSources = dataProps.dataSources.keys
+        ),
         i18n = i18nProps
     )
 }
