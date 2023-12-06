d622abfbad474d0f99140142fb70d8fa0d3e42c3
Boris Chernysh <bchernysh@lanit.ru>
2023-12-06 22:46:11 +0700

    feat: Cache reload indicator

--- src/main/kotlin/ru/scisolutions/scicmscore/api/graphql/ReloadIndicator.kt
+++ src/main/kotlin/ru/scisolutions/scicmscore/api/graphql/ReloadIndicator.kt
@@ -1,23 +1,26 @@
 package ru.scisolutions.scicmscore.api.graphql
 
 import com.netflix.graphql.dgs.internal.DefaultDgsQueryExecutor.ReloadSchemaIndicator
+import org.redisson.api.RedissonClient
 import org.springframework.stereotype.Component
 
 @Component
-class ReloadIndicator : ReloadSchemaIndicator {
-    @Volatile
-    private var isNeedReloadOnce = false
+class ReloadIndicator(redissonClient: RedissonClient) : ReloadSchemaIndicator {
+    private var indicator = redissonClient.getAtomicLong(RELOAD_INDICATOR_NAME)
 
-    fun setNeedReloadOnce(isNeedReloadOnce: Boolean) {
-        this.isNeedReloadOnce = isNeedReloadOnce
+    init {
+        if (!indicator.isExists)
+            indicator.set(0)
     }
 
-    override fun reloadSchema(): Boolean {
-        if (isNeedReloadOnce) {
-            isNeedReloadOnce = false
-            return true
-        }
+    fun setNeedReloadOnce(isNeedReloadOnce: Boolean) {
+        indicator.set(if (isNeedReloadOnce) 1 else 0)
+    }
 
-        return false
+    override fun reloadSchema(): Boolean =
+        indicator.get() == 1L
+
+    companion object {
+        private const val RELOAD_INDICATOR_NAME = "scicms_need_reload"
     }
 }
